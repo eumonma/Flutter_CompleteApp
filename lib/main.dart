1f5faa1c647adff0626ba8748ad1ec1501b6978d
Eugenio Montoro <eugenio.montoro@gmail.com>
2021-08-27 19:03:35 +0200

    incorporación de Firebase. Despacho

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:complete_app/app/sing_in/sign_in_page.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
-void main(){
+//void main(){
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
 //  const MyApp({Key? key}) : super(key: key);
+
+  // Para importar algo Analytics
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+  FirebaseAnalyticsObserver(analytics: analytics);
 
   @override
   Widget build(BuildContext context) {
